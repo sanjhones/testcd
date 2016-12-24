f012bedc30cee5720355758dfe2ce3767995c40a
murugan <murugan@murugans-Mac-mini.local>
2016-12-24 23:36:42 +0530

    Initial Commit

---
+++ Core_Data_Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Core_Data_Test
+//
+//  Created by murugan on 24/12/16.
+//  Copyright © 2016 murugan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Core_Data_Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Core_Data_Test
+//
+//  Created by murugan on 24/12/16.
+//  Copyright © 2016 murugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
